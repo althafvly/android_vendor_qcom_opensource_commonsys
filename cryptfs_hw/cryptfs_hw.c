4d925888777b0ea08f67fbeeb20cc4512ca00a89
althafvly <althafvly@gmail.com>
2020-12-13 23:54:09 +0530

    Revert "cryptfs_hw: Support devices use metadata as key"
    This reverts commit fe8598223a706afba6e7bae4a2d1ca81b6139616.

--- cryptfs_hw/cryptfs_hw.c
+++ cryptfs_hw/cryptfs_hw.c
@@ -68,9 +68,7 @@
 
 #define CRYPTFS_HW_ALGO_MODE_AES_XTS 			0x3
 
-#ifndef USE_METADATA_FOR_KEY
 #define METADATA_PARTITION_NAME "/dev/block/bootdevice/by-name/metadata"
-#endif
 
 enum cryptfs_hw_key_management_usage_type {
 	CRYPTFS_HW_KM_USAGE_DISK_ENCRYPTION		= 0x01,
@@ -367,7 +365,6 @@ int is_ice_enabled(void)
   int storage_type = 0;
   int fd;
 
-#ifndef USE_METADATA_FOR_KEY
   /*
    * Since HW FDE is a compile time flag (due to QSSI requirements),
    * this API conflicts with Metadata encryption even when ICE is
@@ -379,7 +376,6 @@ int is_ice_enabled(void)
     SLOGI("Metadata partition, returning false");
     return 0;
   }
-#endif
 
   if (property_get("ro.boot.bootdevice", prop_storage, "")) {
     if (strstr(prop_storage, "ufs")) {
